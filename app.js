c204d610fecbebeabc1b2aa44872ea758e61375e
ramihlioui <ramihlioui2@gmail.com>
2023-03-25 10:41:53 +0100

    committed

--- app.js
+++ app.js
@@ -17,6 +17,6 @@ app.use(
 app.use(bodyParser({ extended: true }));
 app.use(bodyParser.urlencoded());
 
-app.use("/api/api/routers/amin", require("./routers/api"))
+app.use("/api/api/routers/rami-was-here", require("./routers/api"))
 
 module.exports = app
